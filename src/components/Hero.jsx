82d06f67a9a42a2cacbc23bc3722b210e2e5a116
unknown <matt.mcquain@gmail.com>
2023-03-14 18:26:53 -0700

    Computers canvas properly scales for mobile now. Starting on the About component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,6 +32,29 @@ const Hero = () => {
       {/* Canvas */}  
       <ComputersCanvas />
 
+      {/* little gif thing */}
+      <div
+        className='absolute flex xs:bottom-10 bottom-32 w-full justify-center items-center'
+      >
+        <a href='#about'>
+          <div
+            className='flex justify-center items-start w-[35px] h-[64px] rounded-3xl border-4 border-secondary p-2'
+          >
+            <motion.div 
+              animate={{
+                y: [0, 24, 0] //y axis moves 24px up and down
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop',
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
+
     </section>
   )
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei'; //OrbitCont
 
 import CanvasLoader from '../Loader'
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
 
   return (
@@ -21,8 +21,8 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -32,6 +32,31 @@ const Computers = () => {
 // The Computer // 
 // explained at ~50min https://www.youtube.com/watch?v=0fYi8SGA20k
 const ComputersCanvas = () => {
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  // check if mobile via changing 'isMobile' state
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
     return (
     <Canvas
       frameloop='demand'
@@ -47,7 +72,9 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2} //max angle you can move the camera up
           minPolarAngle={Math.PI / 2} //max angle you can move the camera down
         />
-        <Computers />
+        <Computers
+          isMobile={isMobile}          
+        />
       </Suspense>
 
       <Preload all />
