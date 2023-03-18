1bad4d495dadbb6f289e19e093a48b39ef08261f
unknown <matt.mcquain@gmail.com>
2023-03-17 19:18:37 -0700

    Earth displaying correctly above the Contact form. Starting work on the floating stars

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { Suspense } from 'react' //suspending the canvas until the model is loaded
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 
+import CanvasLoader from '../Loader'
+
+// Earth Model
 const Earth = () => {
+  const earth = useGLTF('./planet/scene.gltf')
+
   return (
-    <div>Earth</div>
+    <primitive 
+      object={earth.scene}
+      scale={2.5}
+      position-y={0}
+      rotation-y={0}
+    />
   )
 }
 
-export default Earth
+// Earth Canvas
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop='demand'
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6]
+      }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls 
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+
+        <Earth />
+      </Suspense>
+    </Canvas>
+  )
+}
+
+export default EarthCanvas;
